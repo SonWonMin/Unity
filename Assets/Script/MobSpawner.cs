ecf80dbfa2f949b22ae31980a4a8a5b23fc563f6
SonWonMin <dnjsals0424@naver.com>
2021-12-03 17:49:34 +0900

    유니티 RPG 포트폴리오
    몹 랜덤 좌표 생성 구현중

--- Assets/Script/MobSpawner.cs
+++ Assets/Script/MobSpawner.cs
@@ -16,7 +16,12 @@ public class MobSpawner : MonoBehaviour
     [SerializeField]
     List<GameObject> Created_Obj;
 
-    Vector3 m_SpawnRange;  // 몬스터 생성 위치 설정 (랜덤값으로 일정한 범위 내에서 생성된다. LocalPosition)
+    [SerializeField]
+    float m_SpawnRange;  // 몬스터 생성 위치 설정 (랜덤값으로 일정한 범위 내에서 생성된다. LocalPosition)
+    [SerializeField]
+    float m_StartRange;
+    [SerializeField]
+    float m_MaxRange;
 
 
     void Update()
@@ -28,10 +33,23 @@ public class MobSpawner : MonoBehaviour
             {
                 if (m_Max_Obj_Count > Created_Obj.Count)
                 {
-                    Created_Obj.Add(Instantiate(m_CreateObj, transform.position, Quaternion.identity));
+                    SetSpawnPoint();
+                    GameObject CreateObj = Instantiate(m_CreateObj, new Vector3(transform.position.x + m_SpawnRange, transform.position.y, transform.position.z + m_SpawnRange), Quaternion.identity);
+                    Created_Obj.Add(CreateObj);
                     m_Cycle = 0;
                 }
             }
         }
+    }
+
+    public void SetSpawnPoint()
+    {
+        m_SpawnRange = Random.Range(m_StartRange, m_MaxRange);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, m_SpawnRange);
     }
 }
